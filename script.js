3a042892da42189477a17ac19572342d6fabf4eb
Raffaele Iemma <62607232+raffaeleiemma@users.noreply.github.com>
2023-11-05 21:48:23 +0100

    Completion of functions
    The game seems to work properly. Probably it will need some refactoring and addition of comments.

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
 const getComputerChoice = function () {
   let number = Math.floor(Math.random() * 3 + 1);
 
-  let choice;
+  let selection;
   if (number === 1) {
-    choice = "Rock";
+    selection = "Rock";
   } else if (number === 2) {
-    choice = "Paper";
+    selection = "Paper";
   } else {
-    choice = "Scissors";
+    selection = "Scissors";
   }
-  console.log(choice);
-  return choice;
+  console.log(selection);
+  return selection;
 };
 
-const getPlayerChoice = function (choice) {
-  let playerChoice = choice.toLowerCase();
-  choice = playerChoice.slice(0, 1).toUpperCase() + playerChoice.slice(1);
-  console.log(choice);
-  return choice;
-};
+const playRound = function (playerSelection, computerSelection) {
+  let playerChoice =
+    playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1);
+  console.log(playerChoice);
 
-const play = function (playerSelection, computerSelection) {
-  let result;
-  let playerScore = 0;
-  let computerScore = 0;
-  if (playerSelection === computerSelection) {
-    result = "It's a draw";
-  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    result = "You win: Rock beats scissors";
-    playerScore++;
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    result = "You lose: Paper beats rock";
-    computerScore++;
-  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    result = "You win: Scissors beats paper";
-    playerScore++;
-  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    result = "You lose: Rock beats scissors";
-    computerScore++;
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    result = "You win: Paper beats rock";
-    playerScore++;
-  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    result = "You lose: Scissors beats paper";
-    computerScore++;
+  let playerScore;
+
+  if (playerChoice === computerSelection) {
+    console.log("It's a draw");
+  } else if (playerChoice === "Rock" && computerSelection === "Scissors") {
+    playerScore = true;
+    console.log("You win: Rock beats scissors");
+  } else if (playerChoice === "Rock" && computerSelection === "Paper") {
+    playerScore = false;
+    console.log("You lose: Paper beats rock");
+  } else if (playerChoice === "Scissors" && computerSelection === "Paper") {
+    playerScore = true;
+    console.log("You win: Scissors beats paper");
+  } else if (playerChoice === "Scissors" && computerSelection === "Rock") {
+    playerScore = false;
+    console.log("You lose: Rock beats scissors");
+  } else if (playerChoice === "Paper" && computerSelection === "Rock") {
+    playerScore = true;
+    console.log("You win: Paper beats rock");
+  } else if (playerChoice === "Paper" && computerSelection === "Scissors") {
+    playerScore = false;
+    console.log("You lose: Scissors beats paper");
   }
-  console.log(result);
-
-  return result;
+  console.log(playerScore);
+  return playerScore;
 };
+
 const game = function () {
-  let totalPlayer;
-  let totalComputer;
+  let playerTotal = 0;
+  let computerTotal = 0;
+  let result;
   for (let i = 0; i < 5; i++) {
-    play(getPlayerChoice("paper"), getComputerChoice());
+    let getPlayerChoice = prompt("Choose rock, paper or scissors");
+    console.log(getPlayerChoice);
+    let result = playRound(getPlayerChoice, getComputerChoice());
+    console.log(result);
+    console.log(i);
+    if (result === true) {
+      playerTotal++;
+    } else if (result === false) {
+      computerTotal++;
+    }
   }
+  if (playerTotal > computerTotal) {
+    console.log("Congrats! You won the game");
+  } else if (playerTotal < computerTotal) {
+    console.log("Sorry, you lost the game");
+  } else {
+    console.log("It's a draw");
+  }
+  console.log(playerTotal, computerTotal);
 };
+
 game();
